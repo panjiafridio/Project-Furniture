d47492592b5be5be23e2ff07c0d8d681fd54b81c
panjiafridio <panjiafridio321@gmail.com>
2022-01-31 14:30:43 +0700

    Project Telah selesai dan siap dikembangkan

---
+++ script.js
@@ -0,0 +1,13 @@
+let rellax = new Rellax('.rellax');
+
+// const tambah = document.querySelector('.tambah')
+// const minus = document.querySelector('.minus')
+// const angka = document.querySelector('.angka')
+
+// let urutanAngka = ["1", "2", "3", "4", "5"];
+// tambah.addEventListener('click', function(){
+//     const angkaBaru = document.createElement("div")
+//     angkaBaru.classList.add('plus')
+
+//     angkaBaru
+// })
